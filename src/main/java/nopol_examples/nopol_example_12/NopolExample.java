75f98dbf65d4d5e62dda58bdc21f34393bab8b34
repairnator <noreply@github.com>
2020-02-05 16:29:00 +0100

    Proposal for a patch

--- src/main/java/nopol_examples/nopol_example_12/NopolExample.java
+++ src/main/java/nopol_examples/nopol_example_12/NopolExample.java
@@ -3,8 +3,13 @@ public class NopolExample {
     public boolean isEmpty(java.util.List list) {
         java.util.ArrayList list2 = new java.util.ArrayList();
         int x = 3; // 3 should also be added to the list of constants
-        if (list.isEmpty())
+        if (list == null) {
+            if ( list2.isEmpty())
             return true;
+        } else {
+            if (list.isEmpty())
+            return true;
+        }
         foo(null);
         int y = x + list2.size(); // required for preventing the compiler to remive the variable (collection is done at ru  qntime)
         return false;
